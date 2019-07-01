45dbbe1ca67003e6853bc1b92ae580f6af8e4d83
danaerodriguez96 <danaerodriguez96@gmail.com>
2019-07-01 19:35:49 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,9 +1,5 @@
 def find_element_index(array, value_to_find)
   print array.index(0) 
-value_to_find.each_with_index do |array, value_to_find|
-  puts "The index is #{array}"
-  puts "The value is #{value_to_find}"
-end
 end
 
 def find_max_value(array)
