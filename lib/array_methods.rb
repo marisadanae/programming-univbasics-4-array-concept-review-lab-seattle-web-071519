95ec1488dfafcf0d126df95efac583ca811d3ffa
danaerodriguez96 <danaerodriguez96@gmail.com>
2019-07-01 18:25:12 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,12 +1,5 @@
 def find_element_index(array, value_to_find)
-counter = 0
-array = [1, 2, 3, 4, 5]
 
-while array[counter] do
-  puts array[counter]
-  counter += 1
-end
-puts array.index()
 end
 
 
