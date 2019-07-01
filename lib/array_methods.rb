5c503e0fe44f6c572e6519f606f0c767c20631a1
danaerodriguez96 <danaerodriguez96@gmail.com>
2019-07-01 20:14:42 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,9 +1,9 @@
 def find_element_index(array, value_to_find)
   array.index(0)
   array.index(500)
-  value_to_find = 0 
+  value_to_find = 0
   array = [0..1]
-  while array[value_to_find] do 
+  while array[value_to_find] do
     puts array[value_to_find]
     counter += 1
 end
